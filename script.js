72037fcb2afa0844ec071adde3872386aef16a26
Faisal Chakiki <faisalchakiki012018@gmail.com>
2022-09-02 20:38:38 +0700

    repair javascript and set responsive

--- script.js
+++ script.js
@@ -26,90 +26,25 @@ titikTiga.addEventListener("click", function () {
   action.classList.toggle("hidden");
 });
 
-// Action Uncheklist Tugas
-const clickDropdownTugas = document.querySelectorAll(".icon-dropdown");
-clickDropdownTugas[1].addEventListener("click", function () {
-  const arrowIcon = document.querySelectorAll(".icon-dropdown");
-  arrowIcon[1].classList.toggle("hidden");
-  const arrowIcons = document.querySelectorAll(".icon-dropup");
-  arrowIcons[1].classList.toggle("hidden");
-  const Unchecklist = document.querySelector(".uncheklist");
-  Unchecklist.classList.toggle("hidden");
-  const titikTiga = document.querySelector(".titik-tiga");
+// Action Uncheklist 
+function uncheklistTugas(dropdown, dropup, result, items) {
+  const dropDown = document.getElementById(dropdown);
+  const dropUp = document.getElementById(dropup);
+  const resultUncheklist = document.getElementById(result);
+  const titikTiga = document.getElementById(items);
+  dropDown.classList.toggle("hidden");
+  dropUp.classList.toggle("hidden");
+  resultUncheklist.classList.toggle("hidden");
   titikTiga.classList.toggle("hidden");
-});
-
-const clickDropUpTugas = document.querySelectorAll(".icon-dropup");
-clickDropUpTugas[1].addEventListener("click", function () {
-  const arrowIcon = document.querySelectorAll(".icon-dropdown");
-  arrowIcon[1].classList.toggle("hidden");
-  const arrowIcons = document.querySelectorAll(".icon-dropup");
-  arrowIcons[1].classList.toggle("hidden");
-  const Unchecklist = document.querySelector(".uncheklist");
-  Unchecklist.classList.toggle("hidden");
-  const titikTiga = document.querySelector(".titik-tiga");
-  titikTiga.classList.toggle("hidden");
-});
-
-// function ActionUncheklist(){
-//     const clickDropdownTugas = document.querySelectorAll(".icon-dropdown");
-//     for ( x = 0 ; x <= clickDropdownTugas.length ;x++ ){
-//         console.log(clickDropdownTugas[x])
-//     }
-// }
-
-// const DropdownTugas = document.querySelectorAll(".icon-dropdown");
-// for (var i = 0; i < DropdownTugas.length; i++) {
-//   DropdownTugas[i].addEventListener("click", function () {
-//     const DropupTugas = document.querySelectorAll(".icon-dropup");
-//     if (this.classList.contains('hidden') === false) {
-//       this.classList.toggle("hidden")
-//       DropupTugas[i].classList.toggle("hidden")
-//     } else {
-//       this.classList.toggle("hidden")
-//       DropupTugas[i].classList.toggle("hidden")
-//     }
-//   });
-// }
-
-// Action See Complited
-const clickComplit = document.querySelectorAll(".icon-dropdown");
-clickComplit[4].addEventListener("click", function () {
-  const arrowIcon = document.querySelectorAll(".icon-dropdown");
-  arrowIcon[4].classList.toggle("hidden");
-  const arrowIcons = document.querySelectorAll(".icon-dropup");
-  arrowIcons[4].classList.toggle("hidden");
-  const hiddShow = document.querySelector(".see-complited");
-  hiddShow.classList.toggle("hidden");
-});
-
-const clickDropUpComplit = document.querySelectorAll(".icon-dropup");
-clickDropUpComplit[4].addEventListener("click", function () {
-  const arrowIcon = document.querySelectorAll(".icon-dropdown");
-  arrowIcon[4].classList.toggle("hidden");
-  const arrowIcons = document.querySelectorAll(".icon-dropup");
-  arrowIcons[4].classList.toggle("hidden");
-  const hiddShow = document.querySelector(".see-complited");
-  hiddShow.classList.toggle("hidden");
-});
+}
 
 // Action Click Line through
-const inputChekbox = document.querySelectorAll(".click-checkbox");
-for (q = 0; q < inputChekbox.length; q++) {
-
-  inputChekbox[q].addEventListener("click", function () {
-
-    this.classList.toggle("line-through");
-
-    const header6 = document.getElementsByClassName(".nama-tugas").this;
-   
-      if (this.checked == true) {
-        header6.classList.add("line-through");
-      } else {
-        header6.remove("line-through");
-      }
-    
-
-  });
-
+function clickCoret(chek, text) {
+  const checkbox = document.getElementById(chek);
+  const textLineThrough = document.getElementById(text);
+  if (checkbox.checked == true) {
+    textLineThrough.classList.add("line-through");
+  } else {
+    textLineThrough.classList.remove("line-through");
+  }
 }
